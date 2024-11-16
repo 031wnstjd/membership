ea155154739a6840fb94d3ce477e0f08135eb22f
junsung <junsung@DESKTOP-2LJAK2G>
2024-11-16 21:23:52 +0900

    서비스 계층 개발

--- src/main/java/com/chu/atdd/membership/app/membership/repository/MembershipRepository.java
+++ src/main/java/com/chu/atdd/membership/app/membership/repository/MembershipRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
-
     Membership findByUserIdAndMembershipType(final String userId, final MembershipType membershipType);
     List<Membership> findAllByUserId(final String userId);
 }
